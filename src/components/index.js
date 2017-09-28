ff11eb128756097cc303ac7b0465ff4fd1478d6b
simonguozirui <simonguozirui@gmail.com>
2017-09-28 16:46:13 -0400

    set up react app, connected to firebase and able to sign up, sign in, enter dashboard

---
+++ src/components/index.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react'
+import { Route, BrowserRouter, Link, Redirect, Switch } from 'react-router-dom'
+import Login from './Login'
+import Register from './Register'
+import Home from './Home'
+import Dashboard from './protected/Dashboard'
+import { logout } from '../helpers/auth'
+import { firebaseAuth } from '../config/constants'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+function PrivateRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+
+function PublicRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to='/dashboard' />}
+    />
+  )
+}
+
+export default class App extends Component {
+  state = {
+    authed: false,
+    loading: true,
+  }
+  componentDidMount () {
+    this.removeListener = firebaseAuth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+          loading: false,
+        })
+      } else {
+        this.setState({
+          authed: false,
+          loading: false
+        })
+      }
+    })
+  }
+  componentWillUnmount () {
+    this.removeListener()
+  }
+  render() {
+    return this.state.loading === true ? <h1>Loading</h1> : (
+      <BrowserRouter>
+        <div>
+          <Navbar color="faded" light toggleable>
+            <NavbarToggler right onClick={this.toggle} />
+            <NavbarBrand href="/">World Affairs Conference</NavbarBrand>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/dashboard">Dashboard</NavLink>
+                </NavItem>
+                <NavItem>
+                  <li>
+                    {this.state.authed
+                      ? <button
+                          style={{border: 'none', background: 'transparent'}}
+                          onClick={() => {
+                            logout()
+                          }}
+                          className="navbar-brand">Logout</button>
+                      : <span>
+                          <Link to="/login" className="navbar-brand">Login</Link>
+                          <Link to="/register" className="navbar-brand">Register</Link>
+                        </span>}
+                  </li>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+
+          <div className="container">
+            <div className="row">
+              <Switch>
+                <Route path='/' exact component={Home} />
+                <PublicRoute authed={this.state.authed} path='/login' component={Login} />
+                <PublicRoute authed={this.state.authed} path='/register' component={Register} />
+                <PrivateRoute authed={this.state.authed} path='/dashboard' component={Dashboard} />
+                <Route render={() => <h3>No Match</h3>} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}

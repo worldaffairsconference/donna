adaa3a7f8f2dc340ced6ffb63c943da369f63c3f
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-09-29 19:52:04 -0400

    amelia

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome'
+
+const ulStyle = {
+	'list-style-type': 'none'
+}
 
 export default class Home extends Component {
+
+
   render () {
     return (
-      <div>
-        Public content of WAC.
+      <div className="container">
+      	<br/>
+        <div className="jumbotron">
+        	<h1 className="display-5">Welcome to WAC!</h1>
+        	<p className="lead">This is the registration site. Please log in to register students. If you're not registered, <a href="/register">sign up</a>.</p>
+        	<hr className="my-4" />
+	        <p className="lead">
+	        	<ul style={ulStyle}>
+				    	<li><a className="btn btn-primary btn-lg" href="/login" role="button">Log in</a></li><br/>
+				    	<li><a className="btn btn-primary btn-lg" href="/register" role="button">Register</a></li>
+				    </ul>
+				  </p>
+        </div>
+        <center>
+	        <div className="btn-group mr-2" role="group" aria-label="First group">
+				    <a className="btn btn-primary" href="https://www.facebook.com/worldaffairsconference/">Facebook</a>
+				    <a className="btn btn-primary" href="https://twitter.com/worldaffairscon">Twitter</a>
+				    <a className="btn btn-primary" href="https://www.instagram.com/worldaffairscon/">Instagram</a>
+				  </div>
+			  </center>
       </div>
     )
   }

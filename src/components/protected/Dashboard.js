23a990d6f37a6c6452939d0a647b181c4ba0f8f8
simonguozirui <simonguozirui@gmail.com>
2017-10-02 00:55:16 -0400

    pull data from specific user's students, need some work processing the object  and load it into the table

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import { Table } from 'reactstrap';
 import { StudentRow } from  './StudentRow'
-
+import firebase from 'firebase'
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
+    var myStudentData = null;
+
+    var userId = firebase.auth().currentUser.uid;
+    firebase.database().ref('users/' + userId + '/students/').once("value", function(snapshot) {
+      myStudentData = snapshot.val();
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
     this.state = {
-      studentDataSet: [{name: "Simon", grade:"11",panel:[1,2,3,4], accessability: "handicapped"}]
+      studentDataSet: [{name: "Simon", grade:"11",panel:[1,2,3,4], accessability: "tired"},
+      {name: "Nick", grade:"11",panel:[2,3,4,5], accessability: "sleepy"}
+    ]
     }
+    console.log(myStudentData);
   }
   render () {
     return (

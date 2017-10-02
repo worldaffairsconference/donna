d477f0a14e14d90ce81099e199f254dba7515eac
simonguozirui <simonguozirui@gmail.com>
2017-10-01 23:37:51 -0400

    create studentrow component for the table to manage student

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import { Table } from 'reactstrap';
+import { StudentRow } from  './StudentRow'
+
 
 export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentDataSet: [{name: "Simon", grade:"11",panel:[1,2,3,4], accessability: "handicapped"}]
+    }
+  }
   render () {
     return (
       <div>
-		<h2>Dashboard</h2>
-		<Link to="/add">Add Students</Link>
-		<hr/>
-		<h3>Students</h3>
+    		<h2>Dashboard</h2>
+    		<Link to="/add">Add Students</Link>
+    		<hr/>
+    		<h3>Students</h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Grade</th>
+              <th>Panel 1</th>
+              <th>Panel 2</th>
+              <th>Panel 3</th>
+              <th>Panel 4</th>
+              <th>Accessability</th>
+            </tr>
+          </thead>
+          <tbody>
+            <StudentRow studentData={this.state.studentDataSet} />
+          </tbody>
+      </Table>
       </div>
     )
   }
---
+++ src/components/protected/StudentRow.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Table } from 'reactstrap';
+
+export function StudentRow(props) {
+      const content = props.studentData.map((student) =>
+      <tr>
+        <td>{student.name}</td>
+        <td>{student.grade}</td>
+        <td>{student.panel[0]}</td>
+        <td>{student.panel[1]}</td>
+        <td>{student.panel[3]}</td>
+        <td>{student.panel[4]}</td>
+        <td>{student.accessability}</td>
+      </tr>
+     );
+
+    return (
+      <div>
+       {content}
+      </div>
+    );
+}

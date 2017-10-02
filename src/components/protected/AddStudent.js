243eadc91b0637eace365319989b995fb292ab9d
simonguozirui <simonguozirui@gmail.com>
2017-10-02 00:30:50 -0400

    succesfully write to firebase db with student info, still need to fix redirect button, check box, and choice limitation

--- src/components/protected/AddStudent.js
+++ src/components/protected/AddStudent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { hashHistory } from 'react-router-dom'
 import Speakers from '../img/speaker-medley.png';
 import { FormGroup, Label, Input } from "reactstrap";
 import firebase from "firebase";
@@ -62,35 +63,47 @@ export default class AddStudent extends Component {
    this.setState({
      panel1: event.target.value,
    });
+	 this.state.panel.push('1');
   }
 	handleChangePanel2(event) {
    this.setState({
      panel2: event.target.value,
    });
+	 this.state.panel.push('2');
   }
 	handleChangePanel3(event) {
    this.setState({
      panel3: event.target.value,
    });
+	 this.state.panel.push('3');
   }
 	handleChangePanel4(event) {
    this.setState({
      panel4: event.target.value,
    });
+	 this.state.panel.push('4');
   }
 	handleChangePanel5(event) {
    this.setState({
      panel5: event.target.value,
    });
+	 this.state.panel.push('5');
   }
 	handleChangePanel6(event) {
    this.setState({
      panel6: event.target.value,
    });
+	 this.state.panel.push('6');
   }
 	handleSubmitStudent(event) {
      event.preventDefault();
-     this.writeStudentData("simon","UCC","11","Handicapped","[1,2,3,4]");
+     this.writeStudentData(
+			 this.state.name,
+			 this.state.school,
+			 this.state.grade,
+			 this.state.accessability,
+			 this.state.panel
+		 );
    }
 
 	 writeStudentData(name, school, grade, accessability, panel) {
@@ -113,11 +126,11 @@ export default class AddStudent extends Component {
 						<div className="card-block">
 							<form onSubmit={this.handleSubmitStudent}>
 								<div className="form-group">
-									<FormGroup>
+									<FormGroup onChange={this.handleChangeName}>
 					          <Label for="studentName">Name</Label>
 					          <Input type="text" name="name" id="studentName" placeholder="Student Name" />
 					        </FormGroup>
-									<FormGroup>
+									<FormGroup onChange={this.handleChangeGrade}>
 							      <Label for="exampleSelect">Grade</Label>
 							      <Input type="select" name="select" id="exampleSelect">
 							        <option>8</option>
@@ -127,50 +140,51 @@ export default class AddStudent extends Component {
 							        <option>12</option>
 							      </Input>
 							    </FormGroup>
-									<FormGroup>
+									<FormGroup onChange={this.handleChangeAccessability}>
 					          <Label for="accessability">Accessability Issues</Label>
 					          <Input type="text" name="name" id="accessability" placeholder="If so, please describe." />
 					        </FormGroup>
+									{/* {this.state.panel} */}
 									<FormGroup check>
 										<Label>Choose plenaries (pick 4 of the 6) </Label>
 					        </FormGroup>
-									<FormGroup check>
+									<FormGroup check onChange={this.handleChangePanel1}>
 										 <Label check>
 											 <Input type="checkbox" />{' '}
 											 	Panel 1
 										 </Label>
 									 </FormGroup>
-									 <FormGroup check>
+									 <FormGroup check onChange={this.handleChangePanel2}>
  										 <Label check>
  											 <Input type="checkbox" />{' '}
  											 	Panel 2
  										 </Label>
  									 </FormGroup>
-									 <FormGroup check>
+									 <FormGroup check onChange={this.handleChangePanel3}>
  										 <Label check>
  											 <Input type="checkbox" />{' '}
  											 	Panel 3
  										 </Label>
  									 </FormGroup>
-									 <FormGroup check>
+									 <FormGroup check onChange={this.handleChangePanel4}>
  										 <Label check>
  											 <Input type="checkbox" />{' '}
  											 	Panel 4
  										 </Label>
  									 </FormGroup>
-									 <FormGroup check>
+									 <FormGroup check onChange={this.handleChangePanel5}>
  										 <Label check>
  											 <Input type="checkbox" />{' '}
  											 	Panel 5
  										 </Label>
  									 </FormGroup>
-									 <FormGroup check>
+									 <FormGroup check onChange={this.handleChangePanel6}>
  										 <Label check>
  											 <Input type="checkbox" />{' '}
  											 	Panel 6
  										 </Label>
  									 </FormGroup>
-									<button type="submit" className="btn btn-primary">Add Student</button>
+									 <button type="submit" className="btn btn-primary" onClick={() => hashHistory.push(`/dashboard`)}>Add Student</button>
 								</div>
 							</form>
 						</div>

183aa09bb734e1a7c2aa1991f93d55b9c25e2c65
simonguozirui <simonguozirui@gmail.com>
2017-10-28 15:49:07 -0400

    change payment area design and add payment instructions

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Table, Alert, Container, Row, Col } from 'reactstrap';
+import { Badge, Table, Alert, Container, Row, Col } from 'reactstrap';
 import { StudentRow } from  './StudentRow'
 import firebase from 'firebase'
 import AddStudent from './AddStudent'
@@ -29,7 +29,7 @@ export default class Dashboard extends Component {
     }
 
     // console.log(this.state.myStudentDataKey);
-    console.log(this.state.myStudentDataArr);
+    // console.log(this.state.myStudentDataArr);
 
     firebase.database().ref('users/' + userId + '/info/').once('value', (snapshot) => this.setState({
       payment: snapshot.val().payment,
@@ -39,18 +39,25 @@ export default class Dashboard extends Component {
     return (
       <Container>
         <br/>
-    		<h2 className="fonted-h">Dashboard</h2>
+    		<h2 className="fonted-h">Teacher Dashboard</h2>
     		<br/>
         {this.state.payment
-            ?  <Alert color="success" id="alert">
-                  <center>You have submitted your payment.</center>
-                </Alert>
-            : <Alert color="danger" id="alert">
-                <center>Please submit payment.</center>
-              </Alert>
+            ? <div><h2>Payment Status: <br /><Badge color="success">Received</Badge></h2></div>
+            : <Row>
+              <Col xs="4">
+              <h2>Payment Status: <br /><Badge color="danger">Not Received</Badge></h2>
+              </Col>
+              <Col xs="8">
+                <h2>Payment Instruction:</h2>
+                <h5>The ticket price for the World Affairs Conference 2018 is $50 per student.
+                <br />Financial Aids are available upon requests.
+                <br />Please send a cheque to Upper Canada College, by Dec 2018.
+                <br /><a href="mailto:wac@ucc.on.ca">Contact Us</a> if you have any question.</h5>
+              </Col>
+              </Row>
           }
         <br/>
-    		<h3>My Students</h3>
+    		<h2>My Students</h2>
         <AddStudent />
         <br />
         <div id="table">

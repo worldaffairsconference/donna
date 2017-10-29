274f3b73de1d38df612f53340b59fa18b297b6e8
simonguozirui <simonguozirui@gmail.com>
2017-10-29 18:19:10 -0400

    make payment status area responsive and add texts for success payment

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -42,18 +42,28 @@ export default class Dashboard extends Component {
     		<h2 className="fonted-h">Teacher Dashboard</h2>
     		<br/>
         {this.state.payment
-            ? <div><h2>Payment Status: <br /><Badge color="success">Received</Badge></h2></div>
+            ? <Row>
+                <Col md="4" sm="12" xs="12">
+                <h2>Payment Status: <br /><Badge color="success">Received</Badge></h2>
+                <br />
+                </Col>
+                <Col md="8" sm="12" xs="12">
+                  <h3>We have received your payment.
+                  <br />See you at WAC 2018!</h3>
+                </Col>
+              </Row>
             : <Row>
-              <Col xs="4">
-              <h2>Payment Status: <br /><Badge color="danger">Not Received</Badge></h2>
-              </Col>
-              <Col xs="8">
-                <h3>Payment Instruction:</h3>
-                <p>The ticket price for the World Affairs Conference 2018 is $45 per student before Jan 9th, 2018, $50 per student after Jan 9th, 2018.
-                <br />Financial Aid is available upon requests.
-                <br />Please send a cheque to Mr. Gregory McDonald, Upper Canada College, 200 Lonsdale Rd, Toronto, ON M4V 1W6.
-                <br /><a href="mailto:wac@ucc.on.ca">Contact Us</a> if you have any question.</p>
-              </Col>
+                <Col md="4" sm="12" xs="12">
+                <h2>Payment Status: <br /><Badge color="danger">Not Received</Badge></h2>
+                <br />
+                </Col>
+                <Col md="8" sm="12" xs="12">
+                  <h3>Payment Instruction:</h3>
+                  <p>The ticket price for the World Affairs Conference 2018 is $45 per student before Jan 9th, 2018, $50 per student after Jan 9th, 2018.
+                  <br />Financial Aid is available upon requests.
+                  <br />Please send a cheque to Mr. Gregory McDonald, Upper Canada College, 200 Lonsdale Rd, Toronto, ON M4V 1W6.
+                  <br /><a href="mailto:wac@ucc.on.ca">Contact Us</a> if you have any question.</p>
+                </Col>
               </Row>
           }
         <br/>

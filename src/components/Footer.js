889bf9b1b6a27aa25688d8720f1741adc35377a9
Matthew Wang <matthewang.wang@gmail.com>
2017-10-29 23:27:33 -0400

    minor footer fix
    needs more progress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react'
 import FontAwesome from 'react-fontawesome'
-import {Row, Col, Button, ButtonGroup} from 'reactstrap'
+import {Row, Col} from 'reactstrap'
 
-var footerStyle = {
-	backgroundColor:"#F5F5F5"
+var redHeart = {
+	color:"red"
 }
 
 
@@ -11,39 +11,45 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<div className="footer">
-			<footer style={footerStyle}>
-				<br/>
-	            <Row>
-	             	<Col>
-	             		<center>
-	             		<div className="container text-muted">
-	                	<p>
-	                		<h4>World Affairs Conference</h4>
-	                		<ButtonGroup>
-						        <a href="http://world.ac"><Button><FontAwesome name="link"/></Button></a>{' '}
-						        <a href="http://instagram.com/worldaffairscon"><Button>
-						        	<FontAwesome name="instagram"/>
-						        </Button></a>{' '}
-						        <a href="http://facebook.com/worldaffairsconference"><Button>
-						        	<FontAwesome name="facebook"/>
-						        </Button></a>{' '}
-						        <a href="http://twitter.com/worldaffairscon"><Button>
-						        	<FontAwesome name="twitter"/>
-						        </Button></a>{' '}
-						        <a href="http://github.com/worldaffairsconference"><Button>
-						        	<FontAwesome name="github"/>
-						        </Button></a>{' '}
-						    </ButtonGroup>
-	                	</p>
-	                	<p>
-	                		Made with <FontAwesome name="code"/> and <FontAwesome name="heart"/> by the <a href="http://github.com/worldaffairsconference">WAC developement team</a>
-	                	</p>
-	                	</div>
-	                	</center>
-	              	</Col>
-	            </Row>
-	        </footer>
-	        </div>
-    	)
+				<footer>
+					<hr />
+				  <Row>
+				    <Col>
+				      <center>
+				        <div className="container text-muted">
+
+				          <h4>World Affairs Conference</h4>
+				          <p>
+				            <a href="http://world.ac">
+											<FontAwesome name="link" className="fa-2x"/>
+										</a>{' '}
+				            <a href="http://instagram.com/worldaffairscon">
+				              <FontAwesome name="instagram" className="fa-2x"/>
+				            </a>{' '}
+				            <a href="http://facebook.com/worldaffairsconference">
+				              <FontAwesome name="facebook" className="fa-2x"/>
+				            </a>{' '}
+				            <a href="http://twitter.com/worldaffairscon">
+				              <FontAwesome name="twitter" className="fa-2x"/>
+				            </a>{' '}
+				            <a href="http://github.com/worldaffairsconference">
+				              <FontAwesome name="github" className="fa-2x"/>
+				            </a>{' '}
+				          </p>
+				          <p>
+				            Made by the 
+				            <a href="http://github.com/worldaffairsconference">WAC team</a>
+										 with
+				            <FontAwesome name="code"/>
+				             and
+				            <FontAwesome name="heart" style={redHeart}/>
+				          </p>
+				        </div>
+				      </center>
+				    </Col>
+				  </Row>
+				</footer>
+			</div>
+    )
 	}
 }

608a95d100974b7ce2016391af69172a20cc5f13
Jefferson Ding <tyding2004@outlook.com>
2021-02-26 20:11:06 -0500

    fix API import

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
-import API from './API';
 var config;
 
 if (process.env.NODE_ENV == 'development') {
+  const API = require('./API').default;
   config = API;
+  console.log(config);
 } else {
   config = process.env.FIREBASE_APIKEY;
 }
